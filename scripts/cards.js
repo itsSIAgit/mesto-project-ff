3a5c133f87382d0ec7a6308e0fa906a0862f0f6e
Ilya Solovev <srpanmf@yandex.ru>
2024-03-10 10:32:51 +1000

    Эксперименты с производительностью JS

--- scripts/cards.js
+++ scripts/cards.js
@@ -24,3 +24,17 @@ const initialCards = [
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
     }
 ];
+
+initialCards.push(...initialCards);
+initialCards.push(...initialCards);
+// initialCards.push(...initialCards);
+// initialCards.push(...initialCards);
+// initialCards.push(...initialCards);
+// initialCards.push(...initialCards);
+// initialCards.push(...initialCards);
+// initialCards.push(...initialCards);
+// initialCards.push(...initialCards);
+// initialCards.push(...initialCards);
+// initialCards.push(...initialCards);
+// initialCards.push(...initialCards);
+console.log(initialCards.length);
