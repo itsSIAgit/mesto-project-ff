fef7c380d036fd2ecfe67de2347014eb7e810b9e
Ilya Solovev <srpanmf@yandex.ru>
2024-03-13 15:43:55 +1000

    Добавлено: базовая работа кнопки лайка

--- src/components/card.js
+++ src/components/card.js
@@ -1,8 +1,9 @@
-function makeCard(cardTemplate, cardData, delCardBtnF) {
+function makeCard(cardTemplate, cardData, delCardBtnF, likeCardBtnF) {
   const cardToMake = cardTemplate.querySelector('.card').cloneNode('true');
   cardToMake.querySelector('.card__image').src = cardData.link;
   cardToMake.querySelector('.card__title').textContent = cardData.name;
   cardToMake.querySelector('.card__delete-button').onclick = delCardBtnF;
+  cardToMake.querySelector('.card__like-button').onclick = likeCardBtnF;
   return cardToMake;
 };
 
@@ -10,4 +11,8 @@ function deleteCard(evt) {
   evt.target.closest('.card').remove();
 };
 
-export { makeCard, deleteCard };
+function likeCard(evt) {
+  evt.target.classList.toggle('card__like-button_is-active');
+};
+
+export { makeCard, deleteCard, likeCard };
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,10 @@
 import { initialCards } from './cards.js';
-import { makeCard, deleteCard } from '../components/card.js';
+import { makeCard, deleteCard, likeCard } from '../components/card.js';
 import '../pages/index.css';
 
 const cardsPosition = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
 
 initialCards.forEach(item => {
-  cardsPosition.append(makeCard(cardTemplate, item, deleteCard));
+  cardsPosition.append(makeCard(cardTemplate, item, deleteCard, likeCard));
 });
