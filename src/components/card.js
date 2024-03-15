312a90d6c00346644b494173fcb92fed856df630
Ilya Solovev <srpanmf@yandex.ru>
2024-03-15 13:30:21 +1000

    Ревью 1: исправления #2

--- src/components/card.js
+++ src/components/card.js
@@ -1,11 +1,12 @@
 function makeCard(cardTemplate, cardData, deleteCardMetod, likeCardMetod, expandCardImg) {
   const cardToMake = cardTemplate.querySelector('.card').cloneNode('true');
-  cardToMake.querySelector('.card__image').src = cardData.link;
-  cardToMake.querySelector('.card__image').alt = cardData.name;
+  const cardImg = cardToMake.querySelector('.card__image'); 
+  cardImg.src = cardData.link;
+  cardImg.alt = cardData.name;
   cardToMake.querySelector('.card__title').textContent = cardData.name;
   cardToMake.querySelector('.card__delete-button').addEventListener('click', deleteCardMetod);
   cardToMake.querySelector('.card__like-button').addEventListener('click', likeCardMetod);
-  cardToMake.querySelector('.card__image').addEventListener('click', expandCardImg);
+  cardImg.addEventListener('click', expandCardImg);
   return cardToMake;
 };
 
