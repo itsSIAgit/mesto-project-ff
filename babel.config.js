2f6bbef195ac2f6ed464a9d36260cfc83611c85a
Ilya Solovev <srpanmf@yandex.ru>
2024-04-02 01:34:10 +1000

    Убрана совместимость с IE и повышены версии

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,10 @@
 const presets = [
   ['@babel/preset-env', { // какой пресет использовать
     targets: { // какие версии браузеров поддерживать
-      edge: '17',
-      ie: '11',
-      firefox: '50',
-      chrome: '64',
-      safari: '11.1'
+      edge: '100',
+      firefox: '100',
+      chrome: '100',
+      safari: '15.5'
     },
 
     // использовать полифилы для браузеров из свойства target
